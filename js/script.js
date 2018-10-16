d1fa8caa938c5d73ab0f760161767b0038b25cf1
denizenofcode <austin.buie@gmail.com>
2018-10-15 22:10:56 -0400

    Worked on function to take student count and split it into groups of 10

--- js/script.js
+++ js/script.js
@@ -8,10 +8,16 @@ var students = document.getElementsByClassName("student-list");
 
 
 
-// Create a function to hide all of the items in the list excpet for the ten you want to show
+// Create a function to hide all of the items in the list except for the ten you want to show
 // Tip: Keep in mind that with a list of 54 students, the last page will only display four
 
+//This function divides the list of students into 'n' groups of 10.
+function splitIntoTens() {
+  const numberOfChildren = students.childElementCount;
+  console.log(numberOfChildren);
+}
 
+splitIntoTens();
 
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
