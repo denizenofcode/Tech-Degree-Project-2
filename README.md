70767ec7de8c8987095e4c1cbc08da736c99e7a8
denizenofcode <austin.buie@gmail.com>
2018-10-15 22:03:30 -0400

    Started Setting Up variables for DOM elements to later manipulate

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tech-Degree-Project-2
+
+# The intent of this project is to create a working pagination-style webpage.
+# A list of students will be provided in HTML, and then displayed 10 at a time
+# using external JS.
--- js/script.js
+++ js/script.js
@@ -4,11 +4,12 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
+var students = document.getElementsByClassName("student-list");
 
 
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
-// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+// Tip: Keep in mind that with a list of 54 students, the last page will only display four
 
 
 
@@ -20,9 +21,3 @@ FSJS project 2 - List Filter and Pagination
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
-
-
-
-
-
-
